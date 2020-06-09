7c3ab83925f26d8a748668d22cd4596e533d5b0b
Yura <55001212+kosmostar2002@users.noreply.github.com>
2020-06-09 09:51:34 +0300

    Add files via upload

---
+++ LAB3/ListOfBools.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace clas1
+{
+    class ListOfBools
+    {
+        private bool[] List;
+
+        private bool sum;
+
+        public ListOfBools(bool[] _list)
+        {
+            List = _list;
+
+        }
+        
+        public bool this[int index]
+        {
+            get
+            {
+                if (index < List.Length)
+                {
+                    return !List[index];
+                }
+                else
+                {
+                    throw new Exception("Your index is bigger then Length of array");
+                }
+            }
+            set
+            {
+                if (index < List.Length){
+                    List[index] = value;
+                }
+                else
+                {
+                    throw new Exception("Your index is bigger then Length of array");
+                }
+            }
+            }
+
+        public bool Sum
+        {
+            get
+            {
+                sum = false;
+                foreach (bool b in List)
+                {
+                    if (b)
+                        sum = true;
+
+
+                }
+                return sum;
+            }
+        }
+    }
+}
+
---
+++ LAB3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using  clas1;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool[] ls = new bool[] { true, true, false }; 
+            ListOfBools b1 = new ListOfBools(ls);
+            Console.WriteLine(b1[0]);
+            Console.WriteLine(b1.Sum);
+            b1[1] = false;
+            b1[0] = false;
+
+            Console.WriteLine(b1.Sum);
+
+        }
+    }
+}
